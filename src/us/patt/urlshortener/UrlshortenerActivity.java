fb629e017fe14d7572edfe4be713f85ca1b4662e
David Pattison <dspattison@gmail.com>
2012-03-11 20:20:08 -0700

    Got the intent and the form to work

--- src/us/patt/urlshortener/UrlshortenerActivity.java
+++ src/us/patt/urlshortener/UrlshortenerActivity.java
@@ -2,7 +2,11 @@ package us.patt.urlshortener;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UrlshortenerActivity extends Activity {
     /** Called when the activity is first created. */
@@ -14,8 +18,33 @@ public class UrlshortenerActivity extends Activity {
         super.onCreate(savedInstanceState);
         final Intent intent = getIntent();
         System.out.println(intent.getAction());
-        System.out.println(intent.getData());
+        Uri uri = intent.getData();
+        System.out.println(uri);
+        
+        
         setContentView(R.layout.main); 
+        
+        //set url text if available
+        final EditText urlEditField = (EditText) findViewById(R.id.editText1);
+        if (null != urlEditField && null != uri) {
+        	urlEditField.setText(uri.toString());
+        }
+        
+        //set listiner for button click
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	System.out.println("Got here!");
+            	//get the target url
+            	CharSequence uriString = null; 
+            	if (urlEditField != null) {
+            		System.out.println(urlEditField);
+            		uriString = urlEditField.getText();
+            	}
+            	System.out.println(uriString);
+            }
+        });
     }
     
 }
